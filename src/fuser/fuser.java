9a97e28c603112412379163bad68c00dfd733e35
aa <aa@example.com>
2014-10-26 21:26:42 -0700

    Revert "changed the source folder of all files related to fuser to fuser folder"
    This reverts commit eaef24c73aa27125a3a2ef0af27021489dc27233.

--- src/fuser/fuser.java
+++ src/fuser/fuser.java
@@ -11,8 +11,8 @@ public class fuser {
 	static ArrayList<String> numberOfCommitList = new ArrayList<String>();
 
 	public static void main(String[] args) throws Exception {
-		File codeQualityFolder = new File("fuser/codequality");
-		File numberOfCommitsFolder = new File("fuser/numberofcommits");
+		File codeQualityFolder = new File("src/codequality");
+		File numberOfCommitsFolder = new File("src/numberofcommits");
 		File[] listOfCodeQualityFiles = codeQualityFolder.listFiles();
 		File[] listOfCommitFiles = numberOfCommitsFolder.listFiles();
 		for (File file : listOfCodeQualityFiles) {
@@ -55,9 +55,9 @@ public class fuser {
 			ArrayList<String> listOfViolations, ArrayList<String> commitList2)
 			throws Exception {
 		FileWriter writeCqFile = new FileWriter(
-				"fuser/codequality/codequalityoutput.xml");
+				"src/codequality/codequalityoutput.xml");
 		FileWriter writeCommitNumFile = new FileWriter(
-				"fuser/numberofcommits/numberofcommitsoutput.xml");
+				"src/numberofcommits/numberofcommitsoutput.xml");
 
 		BufferedWriter write1 = new BufferedWriter(writeCqFile);
 		BufferedWriter write2 = new BufferedWriter(writeCommitNumFile);
