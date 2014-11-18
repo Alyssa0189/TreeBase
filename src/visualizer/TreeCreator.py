c718a700ea0d2a303d4a248005b4120ff149fdd3
alyssa <alyssa.0189@gmail.com>
2014-11-17 20:52:40 -0800

    Changed tree structure a bit, added height limit

--- src/visualizer/TreeCreator.py
+++ src/visualizer/TreeCreator.py
@@ -19,15 +19,15 @@ badQualityCeiling = 200.0    # Code of quality this bad or greater will result i
 
 # Set the command to evaluate tree creation.  The tree's 'height' should go between these commands.
 
-commandStrBeforeHeight = "bpy.ops.curve.tree_add(do_update=True, chooseSet='0', bevel=True, prune=False, \
-showLeaves=True, useArm=False, seed=0, handleType='1', levels=2, length=(1, 0.3, 0.6, 0.45), \
-lengthV=(0, 0, 0, 0), branches=(0, 50, 30, 10), curveRes=(3, 5, 3, 1), curve=(0, -40, -40, 0), \
-curveV=(20, 50, 75, 0), curveBack=(0, 0, 0, 0), baseSplits=0, segSplits=(0, 0, 0, 0), splitAngle=(0, 0, 0, 0), splitAngleV=(0, 0, 0, 0), \
+commandStrBeforeHeight = "bpy.ops.curve.tree_add(do_update=True, chooseSet='4', bevel=True, prune=False, \
+showLeaves=True, useArm=False, seed=0, handleType='1', levels=3, length=(1, 0.3, 0.6, 0.45), \
+lengthV=(0, 0, 0, 0), branches=(1, 52, 30, 10), curveRes=(3, 5, 3, 1), curve=(0, -40, -40, 0), \
+curveV=(20, 50, 75, 0), curveBack=(0, 0, 0, 0), baseSplits=0, segSplits=(0.01, 0.01, 0.01, 0.01), splitAngle=(0, 0, 0, 0), splitAngleV=(0, 0, 0, 0), \
 scale="
 
-commandStrAfterHeight = ", scaleV=3, attractUp=0.5, shape='7', baseSize=0.4, ratio=0.015, taper=(1, 1, 1, 1), ratioPower=1.2, downAngle=(90, 60, 45, 45), \
+commandStrAfterHeight = ", scaleV=3, attractUp=0.5, shape='7', baseSize=0.26, ratio=0.015, taper=(1, 1, 1, 1), ratioPower=1.2, downAngle=(90, 60, 45, 45), \
 downAngleV=(0, -50, 10, 10), rotate=(140, 140, 140, 77), rotateV=(0, 0, 0, 0), scale0=1, scaleV0=0.2, pruneWidth=0.4, pruneWidthPeak=0.6, \
-prunePowerHigh=0.5, prunePowerLow=0.001, pruneRatio=1, leaves=25, leafScale=0.17, leafScaleX=1, leafDist='4', bend=0, bevelRes=0, resU=4, \
+prunePowerHigh=0.5, prunePowerLow=0.001, pruneRatio=1, leaves=12, leafScale=0.1, leafScaleX=1, leafShape='hex', leafDist='4', bend=0.02, bevelRes=2, resU=4, \
 frameRate=1, windSpeed=2, windGust=0, armAnim=False, startCurv=0)"
 
 
@@ -37,9 +37,15 @@ frameRate=1, windSpeed=2, windGust=0, armAnim=False, startCurv=0)"
 # Param quality: The code quality info, where a lower number creates a more lively tree.
 # 
 def createTree(contribs, quality):
-	treeHeight = ((maxTreeHeight - minTreeHeight) / contribCeiling) 
-	treeHeight *= float(contribs)
-	treeHeight += minTreeHeight
+	# Set tree height to maximum if many contribs
+	if float(contribs) >= contribCeiling:
+		treeHeight = maxTreeHeight
+	
+	else:
+		# Set tree height based on contribs
+		treeHeight = ((maxTreeHeight - minTreeHeight) / contribCeiling) 
+		treeHeight *= float(contribs)
+		treeHeight += minTreeHeight
 
 	treeColor = __getColor__(quality)
 	__makeTree__(treeHeight, treeColor)
